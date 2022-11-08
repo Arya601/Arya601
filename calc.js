5def5293db615bee416892392c01fc905053b485
Arya601 <58103246+Arya601@users.noreply.github.com>
2022-11-09 00:52:53 +0800

    Add files via upload
    add test 2

--- calc.js
+++ calc.js
@@ -1,17 +1,34 @@
  function calc() {
 		
-		let Sred = 0, Sgreen = 0, Sblue = 0, Sblack = 0, Swhite = 0;
+		let Sred = 0, Sgreen = 0, Sblue = 0, Sblack = 0, Swhite = 0,checkbox28;
 		var degreeArray =[],Array=[];
-		for(let i = 0; i < 20; i++)
+		for(let i = 0; i < 30; i++)
 		{
-			degreeArray[i] =document.querySelector('input[name = "degree'+ (i+1) +'"]:checked').value;
+			/*if(!=27)
+			{*/
+				degreeArray[i] =document.querySelector('input[name = "degree'+ (i+1) +'"]:checked').value;
+				
+				Array=degreeArray[i].split("_");
+				Sred += Number(Array[0]); 
+				Sgreen += Number(Array[1]); 
+				Sblue += Number(Array[2]); 
+				Sblack += Number(Array[3]); 
+				Swhite += Number(Array[4]); 
+			/*}
+			else
+			{
+				 degreeArray[i] = document.querySelectorAll('input[name="degree'+ (i+1) +'"]:checked');
+				 degreeArray[i].forEach((checkbox) => {
+					checkbox28 = checkbox.value;
+					Array=checkbox28.split("_");
+					Sred += Number(Array[0]); 
+					Sgreen += Number(Array[1]); 
+					Sblue += Number(Array[2]); 
+					Sblack += Number(Array[3]); 
+					Swhite += Number(Array[4]); 
+					});
+			}*/
 			
-			Array=degreeArray[i].split("_");
-			Sred += Number(Array[0]); 
-            Sgreen += Number(Array[1]); 
-            Sblue += Number(Array[2]); 
-            Sblack += Number(Array[3]); 
-            Swhite += Number(Array[4]); 		
 		}
             document.getElementById("sred").value = Sred;
             document.getElementById("sgreen").value = Sgreen;
