5c80e5a0c4b60f2662115212d2eded45de53e3b9
Arya601 <58103246+Arya601@users.noreply.github.com>
2023-05-08 23:26:14 +0800

    Add files via upload
    用chapgpt增加漏填提醒功能，及優化

--- calc.js
+++ calc.js
@@ -1,38 +1,61 @@
- function calc() {
-		
-		let Sred = 0, Sgreen = 0, Sblue = 0, Sblack = 0, Swhite = 0,checkbox28;
-		var degreeArray =[],Array=[];
-		for(let i = 0; i < 30; i++)
-		{
-			/*if(!=27)
-			{*/
-				degreeArray[i] =document.querySelector('input[name = "degree'+ (i+1) +'"]:checked').value;
-				
-				Array=degreeArray[i].split("_");
-				Sred += Number(Array[0]); 
-				Sgreen += Number(Array[1]); 
-				Sblue += Number(Array[2]); 
-				Sblack += Number(Array[3]); 
-				Swhite += Number(Array[4]); 
-			/*}
-			else
-			{
-				 degreeArray[i] = document.querySelectorAll('input[name="degree'+ (i+1) +'"]:checked');
-				 degreeArray[i].forEach((checkbox) => {
-					checkbox28 = checkbox.value;
-					Array=checkbox28.split("_");
-					Sred += Number(Array[0]); 
-					Sgreen += Number(Array[1]); 
-					Sblue += Number(Array[2]); 
-					Sblack += Number(Array[3]); 
-					Swhite += Number(Array[4]); 
-					});
-			}*/
-			
-		}
-            document.getElementById("sred").value = Sred;
-            document.getElementById("sgreen").value = Sgreen;
-            document.getElementById("sblue").value = Sblue;
-            document.getElementById("sblack").value = Sblack;
-            document.getElementById("swhite").value = Swhite;
+function calc(event) {
+    event.preventDefault(); // 阻止 submit 事件的預設行為
+    const form = document.getElementById("quiz-form");
+
+    // 檢查程度題是否作答
+    const degreeUnanswered = [];
+    for (let i = 1; i <= 20; i++) {
+        const question = form.querySelector(`input[name="degree${i}"]:checked`);
+        if (!question) {
+            degreeUnanswered.push(i);
         }
+    }
+
+    // 檢查選擇題是否作答
+    const choiceUnanswered = [];
+    for (let i = 21; i <= 30; i++) {
+        const question = form.elements[`degree${i}`];
+        const isChecked = Array.from(question).some((input) => input.checked);
+        if (!isChecked) {
+            choiceUnanswered.push(i - 20);
+        }
+    }
+
+    // 如果有未作答的題目，顯示提示訊息
+    if (degreeUnanswered.length > 0 && choiceUnanswered.length > 0) {
+        alert(`以下問題還沒有回答完整喔！\n\n第一大題程度題：${degreeUnanswered.join(", ")}\n第二大題選擇題：${choiceUnanswered.join(", ")}`);
+    } else if (degreeUnanswered.length > 0) {
+        alert(`以下問題還沒有回答完整喔！\n\n第一大題程度題：${degreeUnanswered.join(", ")}`);
+    } else if (choiceUnanswered.length > 0) {
+        alert(`以下問題還沒有回答完整喔！\n\n第二大題選擇題：${choiceUnanswered.join(", ")}`);
+    } else {
+        showResults(form);
+    }
+}
+
+function showResults(form) {
+    const degreeArray = [];
+    for (let i = 1; i <= 30; i++) {
+        const answer = form.querySelector(`input[name="degree${i}"]:checked`);
+        if (answer) {
+            degreeArray.push(answer.value.split("_"));
+        }
+    }
+
+    const sum = degreeArray.reduce(
+        (acc, val) => [
+            acc[0] + Number(val[0]),
+            acc[1] + Number(val[1]),
+            acc[2] + Number(val[2]),
+            acc[3] + Number(val[3]),
+            acc[4] + Number(val[4])
+        ],
+        [0, 0, 0, 0, 0]
+    );
+
+    document.querySelector("#sred-text").innerText = sum[0];
+    document.querySelector("#sgreen-text").innerText = sum[1];
+    document.querySelector("#sblue-text").innerText = sum[2];
+    document.querySelector("#sblack-text").innerText = sum[3];
+    document.querySelector("#swhite-text").innerText = sum[4];
+}
